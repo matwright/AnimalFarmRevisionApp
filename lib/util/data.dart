7479062e17952232b10d2aab377b53a6c6a53ac4
matwright <mat.wright@broadshout.com>
2019-11-01 18:08:52 +0100

    adding character objects

--- lib/util/data.dart
+++ lib/util/data.dart
@@ -9,5 +9,111 @@ List<Map<String, dynamic>> characters = [
   "avatar": "napoleon.png",
   "bio": "Hi, I'm Napoleon",
   "strap_line": "To infinity and beyond!"
-}];
+},
+
+  {
+    "id": "snowball",
+    "name": "snowball",
+    "avatar": "snowball.png",
+    "bio": "Hi, I'm snowball",
+    "strap_line": "To infinity and beyond!"
+  }
+
+  ,
+
+  {
+    "id": "boxer",
+    "name": "boxer",
+    "avatar": "boxer.png",
+    "bio": "Hi, I'm Napoleon",
+    "strap_line": "To infinity and beyond!"
+  }
+  ,
+
+  {
+    "id": "old_major",
+    "name": "old_major",
+    "avatar": "old_major.png",
+    "bio": "Hi, I'm old_major",
+    "strap_line": "To infinity and beyond!"
+  }
+  ,
+
+  {
+    "id": "squealer",
+    "name": "squealer",
+    "avatar": "squealer.png",
+    "bio": "Hi, I'm squealer",
+    "strap_line": "To infinity and beyond!"
+  }
+  ,
+
+  {
+    "id": "moses",
+    "name": "moses",
+    "avatar": "moses.png",
+    "bio": "Hi, I'm Napoleon",
+    "strap_line": "To infinity and beyond!"
+  }
+  ,
+
+  {
+    "id": "farmer_jones",
+    "name": "farmer_jones",
+    "avatar": "farmer_jones.png",
+    "bio": "Hi, I'm Napoleon",
+    "strap_line": "To infinity and beyond!"
+  }
+  ,
+
+  {
+    "id": "benjamin",
+    "name": "benjamin",
+    "avatar": "benjamin.png",
+    "bio": "Hi, I'm benjamin",
+    "strap_line": "To infinity and beyond!"
+  }
+
+  ,
+
+  {
+    "id": "clover",
+    "name": "clover",
+    "avatar": "clover.png",
+    "bio": "Hi, I'm clover",
+    "strap_line": "To infinity and beyond!"
+  }
+
+  ,
+
+  {
+    "id": "mollie",
+    "name": "mollie",
+    "avatar": "mollie.png",
+    "bio": "Hi, I'm mollie",
+    "strap_line": "To infinity and beyond!"
+  }
+
+  ,
+
+
+
+  {
+    "id": "frederick",
+    "name": "frederick",
+    "avatar": "frederick.png",
+    "bio": "Hi, I'm frederick",
+    "strap_line": "To infinity and beyond!"
+  }  ,
+
+
+
+  {
+    "id": "pilkington",
+    "name": "pilkington",
+    "avatar": "pilkington.png",
+    "bio": "Hi, I'm pilkington",
+    "strap_line": "To infinity and beyond!"
+  }
+];
 
